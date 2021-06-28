600bf7b6275f72cec711358249940418183def2d
JavaDevHo <jungho9707@naver.com>
2021-06-28 19:30:53 +0900

    git commit test

--- src/_recursiveFunc/gitprac.java
+++ src/_recursiveFunc/gitprac.java
@@ -2,6 +2,6 @@ package _recursiveFunc;
 // 깃허브 커밋이 정상적으로 되는지 확인
 public class gitprac {
 	public static void main(String[] args) {
-		System.out.println("이클립스 깃허브 연동 확인123");
+		System.out.println("이클립스 깃허브 연동 확인1234");
 	}
 }
